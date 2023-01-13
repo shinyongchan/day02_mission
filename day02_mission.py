816611c161d06fa2493a896ff897a38c5c23edcd
syc <tlsdydcks135@naver.com>
2023-01-13 16:04:20 +0900

    day02_mission02

--- day02_mission.py
+++ day02_mission.py
@@ -1,13 +1,14 @@
-#05
-import random
-secret = random.randint(1,10) # 1에서 10 사이의 정수가 임의로 발생
-guess = int(input('1~10사이의 수 :'))
-if guess>secret :
-    print('too high')
-else :
-    if guess==secret :
-        print('just right')
-    else :
-        print('too low')
+small = True
+green = True
 
+if small:
+    if green:
+        print('완두콩')
+    else:
+        print('체리')
+else:
+    if green:
+        print('수박')
+    else:
+        print('호박')
 
