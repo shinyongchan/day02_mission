6d934eb3296f3ab1962a6e88adb39c31aabe5c45
syc <tlsdydcks135@naver.com>
2023-01-16 16:30:03 +0900

    ch5-ex6

--- day03_mission.py
+++ day03_mission.py
@@ -1,12 +1,8 @@
-#ch5-ex4,5
+#ch5-ex6
 
 
-letter="\tDear {} {},\n\n\tThank you for your letter. We are sorry that our {} {} in your\n\
-  {}. Please note that it should never be used in a {}, especially near any\n\
-  {}\n\n\tSend us your receipt and {} for shipping and handling We will send you\n another {}\
-   that, in our tests, is {}% less likely to have {}.\n\n\tThank you for your support.\n\tSincerely,\n\t{}\n\t{}"
-
-print(letter.format('salutation', 'name', 'product', 'verbed', 'room', 'room', 'animals', 'amount', 'product',\
-                    'percent', 'verbed', 'spokesman', 'job_title'))
+print(('%s'' Mc' '%s''face')%('Duck','Duck'))
+print(('%s'' Mc' '%s''face')%('Gourd','Gourd'))
+print(('%s'' Mc' '%s''face')%('Spitz','Spitz'))
 
 
