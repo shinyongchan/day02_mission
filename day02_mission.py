1d57a83409c9dee38a724fd995ab51f8a97217e4
syc <tlsdydcks135@naver.com>
2023-01-16 09:44:00 +0900

    day02_mission2

--- day02_mission.py
+++ day02_mission.py
@@ -1,5 +1,13 @@
-small = True
-green = True
+import random
+
+small = random.choice([True,False])
+print (small)
+green = random.choice([True,False])
+print (green)
+
+
+# small = True
+# green = True
 
 if small:
     if green:
