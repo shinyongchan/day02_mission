6af2aad54c081dbdb4767e892b513f89fbeb4be5
syc <tlsdydcks135@naver.com>
2023-02-06 23:14:48 +0900

    day16_mission_ch6-1

--- day16_mission.py
+++ day16_mission.py
@@ -1,39 +1,56 @@
-#ch5-2
+#ch6-1
 import random
-import math
 
-class Node:
-	def __init__(self, data):
-		self.data = data
-		self.plink = None
-		self.nlink = None
-def print_node(start):
-	current = start
-	print("정방향 -->", end=' ')
-	print(current.data, end=' ')
-	while current.nlink != None:
-		current = current.nlink
-		print(current.data, end=' ')
-	print()
-	print("역방향 -->", end=' ')
-	print(current.data, end=' ')
-	while current.plink != None:
-		current = current.plink
-		print(current.data, end=' ')
+def isStackFull():
+	global SIZE, stack, top
+	if (top >= SIZE-1):
+		return True
+	return False
+
+def isStackEmpty():
+	global SIZE, stack, top
+	if (top == -1):
+		return True
+	return False
+
+def push(data):
+	global SIZE, stack, top
+	if (isStackFull()):
+		return
+	top = top + 1
+	stack[top] = data
+
+def pop():
+	global SIZE, stack, top
+	if (isStackEmpty()):
+		return
+	data = stack[top]
+	stack[top] = None
+	top = top - 1
+	return data
 
 
-data_array = ['다현', '정연', '쯔위', '사나', '지효']
-head, current, pre = None, None, None
+SIZE = 6
+stack = [None for _ in range(SIZE)]
+top = -1
 
 
 if __name__ == '__main__':
-	node = Node(data_array[0])
-	head = node
 
-	for data in data_array[1:]:
-		pre = node
-		node = Node(data)
-		pre.nlink = node
-		node.plink = pre
+	ston_array=['빨강', '파랑', '초록', '노랑', '보라', '주황']
+	random.shuffle(ston_array)
 
-	print_node(head)
+
+	print("과자집에 가는 길 : ", end = ' ')
+	for ston in ston_array:
+		push(ston)
+		print(ston," -->", end=' ')
+	print("과자집")
+
+	print("우리집에 오는 길 : ", end=' ')
+	while True:
+		ston = pop()
+		if ston == None:
+			break
+		print(ston," -->",end=' ')
+	print("우리집")
