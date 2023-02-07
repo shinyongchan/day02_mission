6de6f22558b9bcd3b6dcc145017d0ff18630eeea
syc <tlsdydcks135@naver.com>
2023-02-07 16:52:15 +0900

    day17_mission_ch7-2

--- day17_mission.py
+++ day17_mission.py
@@ -1,33 +1,45 @@
-Ch7_1
+#Ch7_2
+
+
+def enqueue(data):
+    global SIZE, front, rear
+    if (rear+1) % SIZE == 0:
+        print("큐가 꽉찼습니다.")
+    rear = (rear+1) % SIZE
+    queue[rear] = data
+
 
 def dequeue():
     global SIZE, front, rear
-    if rear == -1:
+    if rear == front:
         print("큐가 비었습니다.")
         return
-    front = front + 1
+    front = (front + 1) % SIZE
     data = queue[front]
     queue[front] = None
-    for i in range(front+1,SIZE):
-        queue[i-1] = queue[i]
-        queue[i] = None
-    rear = rear - 1
-    front = - 1
     return data
 
 
+def calc_time():
+    time_sum = 0
+    for i in range((front+1) % SIZE,(rear+1) % SIZE):
+        time_sum = time_sum + queue[i][1]
+    return time_sum
 
-
-SIZE = 5
-queue = ['정국', '뷔', '지민', '진', '슈가']
-front = -1
-rear = 4
+SIZE = 6
+queue = [None for _ in range(SIZE)]
+front = 0
+rear = 0
 
 if __name__ == "__main__":
-    for _ in range(SIZE):
-        print(f'대기 줄 상태 : {queue}')
-        print(f'{dequeue()} 님 식당에 들어감')
-    print(f'대기 줄 상태 : {queue}')
-    print("식당 영업 종료!")
+    wait_call = [('사용', 9), ('고장', 3), ('환불', 4), ('환불', 4), ('고장', 3)]
 
 
+    for call in wait_call:
+        print(f'귀하의 대기 예상시간은 {calc_time()}분 입니다.')
+        print(f'현재 대기 콜 --> {queue}')
+        print()
+        enqueue(call)
+
+    print(f'최종 대기 콜 --> {queue}')
+    print("프로그램 종료!")
