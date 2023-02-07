8c0c3948b007a3afa855f44dd23936a39a0101d8
syc <tlsdydcks135@naver.com>
2023-02-07 18:43:10 +0900

    day17_mission_ch8-1

--- day17_mission.py
+++ day17_mission.py
@@ -1,45 +1,48 @@
-#Ch7_2
+#Ch8_1
+import random
+class tree_node:
+    def __init__(self, data):
+        self.left = None
+        self.data = data
+        self.right = None
 
-
-def enqueue(data):
-    global SIZE, front, rear
-    if (rear+1) % SIZE == 0:
-        print("큐가 꽉찼습니다.")
-    rear = (rear+1) % SIZE
-    queue[rear] = data
-
-
-def dequeue():
-    global SIZE, front, rear
-    if rear == front:
-        print("큐가 비었습니다.")
+def preorder(node):
+    if node == None:
         return
-    front = (front + 1) % SIZE
-    data = queue[front]
-    queue[front] = None
-    return data
+    print(node.data, end=' ')
+    preorder(node.left)
+    preorder(node.right)
 
 
-def calc_time():
-    time_sum = 0
-    for i in range((front+1) % SIZE,(rear+1) % SIZE):
-        time_sum = time_sum + queue[i][1]
-    return time_sum
-
-SIZE = 6
-queue = [None for _ in range(SIZE)]
-front = 0
-rear = 0
+root = None
+data_array = ['바나나맛우유', '레쓰비', '츄파츕스', '도시락', '삼각김밥', '코카콜라', '삼다수',]
+sell_array = [random.choice(data_array) for _ in range(20)]
 
 if __name__ == "__main__":
-    wait_call = [('사용', 9), ('고장', 3), ('환불', 4), ('환불', 4), ('고장', 3)]
 
+    node = tree_node(sell_array[0])
+    root = node
+    for sell in sell_array[1:]:
+        node = tree_node(sell)
 
-    for call in wait_call:
-        print(f'귀하의 대기 예상시간은 {calc_time()}분 입니다.')
-        print(f'현재 대기 콜 --> {queue}')
-        print()
-        enqueue(call)
-
-    print(f'최종 대기 콜 --> {queue}')
-    print("프로그램 종료!")
+        current = root
+        while True:
+            if sell < current.data:
+                if current.left == None:
+                    current.left = node
+                    break
+                current = current.left
+            elif sell == current.data:
+                break
+            else:
+                if current.right == None:
+                    current.right = node
+                    break
+                current = current.right
+    print("오늘 판매된 물건(중복O) : ", end=' ')
+    print(sell_array)
+    print()
+    print("이진 탐색 트리 구성 완료!")
+    print()
+    print("오늘 판매된 종류(중복X) : ", end=' ')
+    preorder(root)
