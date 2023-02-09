a9339786c5aa1ecdab9e9e4a002d67a23d4d7af5
syc <tlsdydcks135@naver.com>
2023-02-09 16:54:53 +0900

    day19_mission_ch11-1

--- day19_mission.py
+++ day19_mission.py
@@ -1,19 +1,15 @@
-#Ch10_1
-def notation(base, num):
-    if base > num :
-        print(number_char[num], end='')
-    else:
-        notation(base, num//base)
-        print(number_char[num%base],end='')
+#Ch11_1
+def score_sort(ary):
+    for end in range(1, len(ary)):
+        for i in range(end, 0, -1):
+            if ary[i][1] < ary[i-1][1]:
+                ary[i], ary[i-1] = ary[i-1], ary[i]
+    return ary
 
-number_char = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-number_char = number_char + ['A', 'B', 'C', 'D', 'E', 'F']
-num = int(input("10진수 입력 : "))
-print("2진수 : ", end='')
-notation(2, num)
-print()
-print("8진수 : ", end='')
-notation(8, num)
-print()
-print("16진수 : ", end='')
-notation(16, num)
+score_ary = [['선미', 88], ['초아', 99], ['화사', 71], ['영탁', 78], ['영웅', 67], ['민호', 92]]
+print("정렬전 ---> ", score_ary)
+score_sort(score_ary)
+print("정렬후 ---> ", score_ary)
+print("성적별 조 편성표")
+for i in range (len(score_ary)//2):
+    print(f'{score_ary[i][0]} : {score_ary[len(score_ary)-1-i][0]}')
