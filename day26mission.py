44d3645cb7179d02bd1b61f699e940516d359e59
syc <tlsdydcks135@naver.com>
2023-02-20 18:23:26 +0900

    day26mission01

---
+++ day26mission.py
@@ -0,0 +1,42 @@
+import random
+
+
+def bin_search(ary,find_data):
+    start = 0
+    end = len(ary) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if find_data == ary[mid]:
+            return mid
+        elif find_data > ary[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return -1
+
+
+data_ary = ['바나나맛우유', '레쓰비캔커피', '츄파춥스', '도시락', '삼다수', '코카콜라', '삼각김밥']
+sell_ary = [random.choice(data_ary) for _ in range(20)]
+
+
+print('#오늘 판매된 전체 물건(중복O, 정렬X) -->', sell_ary)
+sell_ary.sort()
+print('#오늘 판매된 전체 물건(중복O, 정렬O) -->', sell_ary)
+sell_product = list(set(sell_ary))
+print('#오늘 판매된 물품 종류(중복x) -->', sell_product)
+
+count_list = []
+for product in sell_product:
+    count = 0
+    pos = 0
+    while pos != -1:
+        pos = bin_search(sell_ary, product)
+        if pos != -1:
+            count += 1
+            del(sell_ary[pos])
+    count_list.append((product, count))
+
+print()
+print("결산 결과 ==>", count_list)
